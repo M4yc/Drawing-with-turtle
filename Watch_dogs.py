7ccb1b3e18252bee6356c7d59ca827b335a017c5
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-03-10 12:07:01 +0000

    Create Watch_dogs.py

---
+++ Watch_dogs.py
@@ -0,0 +1,32 @@
+import turtle as t
+
+t.title("Watch Dogs Logo")
+
+t.speed(2)
+t.bgcolor("black")
+t.pensize(6)
+t.color("white")
+
+t.penup()
+t.goto(-50, 0)
+
+t.pendown()
+t.left(90)
+
+t.forward(100)
+t.right(150)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.right(150)
+t.forward(100)
+t.left(210)
+t.forward(100)
+t.right(240)
+t.forward(100)
+
+
+
+t.hideturtle()
+
+t.done()
